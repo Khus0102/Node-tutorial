1b1986da298123733858503c965673d5b5190ced
Kushalbir Singh Padda <kushalbir.singh@icloud.com>
2024-07-05 18:09:37 -0400

    Add new folder

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-const { readFile, writeFile } = require("fs");
+const http = require("http");
 
-readFile("./content/first.txt", "utf8", (err, result) => {
-  if (err) {
-    console.log(err);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to homepage");
     return;
   }
-  const first = result;
-  readFile("./content/second.txt", "utf8", (err, result) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const second = result;
-
-    writeFile(
-      "./content/async-written-file/",
-      `here is the result: ${first}, ${second}`,
-      (err, result) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log(result);
-      }
-    );
-  });
+  if (req.url === "/about") {
+    res.end("here is our history");
+    return; //important as .js keeps reading the coding even after this point
+    //so it will try to res.end the lower command as well, leading to
+    //error:- ERR_STREAM_WEITE_AFTER_END
+  }
+  res.end(`<h1>ohoo!</h1>
+    <p>we can't find your page</p>
+    <a href="/" back home</a>`);
 });
+server.listen(5000);
